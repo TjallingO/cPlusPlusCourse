36b51726a0c96cb31da57e12aae2da314837f6be
TjallingO <tjalling@taikie.com>
2018-09-06 14:36:18 +0200

    First commit

---
+++ Week 1/hello.cc
@@ -0,0 +1,9 @@
+// Programming in C/C++
+// Week 1: Assignment 1
+// Tjalling Otter & Emiel Krol
+
+#include <iostream>
+int main()
+{
+  std::cout << "Hello World" << '\n'; // Printing 'Hello World' to the console
+}
