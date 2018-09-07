4fdcacc90ef6f5a63276a9da12cf6f92749730b9
EmielK <EmielKrol@gmail.com>
2018-09-07 17:15:03 +0200

    ww

--- Week 1/test.cc
+++ Week 1/test.cc
@@ -2,6 +2,6 @@
 
 int main()
 {
-    std::cout << "Example text\xA5" << "More example text" << '\a'<< '\n';                  //Printing text and adding a "\" on the next line
+    std::cout << "Example text\xB5" << "More example text" << '\a'<< '\n';                  //Printing text and adding a "\" on the next line
                                                                   //Since it follows " '\n' "
 }
