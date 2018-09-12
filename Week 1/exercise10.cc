41d623bde28550e8a54c513a3693a35db005c8a6
TjallingO <tjalling@taikie.com>
2018-09-12 21:53:56 +0200

    ex10 done

--- Week 1/exercise10.cc
+++ Week 1/exercise10.cc
@@ -9,13 +9,13 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    size_t value = stoul(argv[1], 0, 16);               // initialize hexadecimal value
-    size_t nibble = stoul(argv[2]);                     // nibble to replace
-    size_t replacement = stoul(argv[3]) % 16;           // new nibble (= 0 .. 15)
+  size_t value = stoul(argv[1], 0, 16);         // initialize hexadecimal value
+  size_t nibble = stoul(argv[2]);               // nibble to replace
+  size_t replacement = stoul(argv[3]) % 16;     // new nibble (= 0 .. 15)
 
-    size_t offsetNibble = replacement << (4 * nibble);  // Shift the new nibble to its desired spot
-    size_t offsetMask = 15 << (4 * nibble);             // Create a mask to zero the nibble to be replaced
-    value = (value & ~offsetMask) | offsetNibble;       // First, zero the nibble to be replaced, then replace it with the new nibble
+  replacement <<= (4 * nibble);                 // Shift the new nibble to its desired spot
+  size_t offsetMask = 0b1111 << (4 * nibble);   // Create a mask to zero the nibble to be replaced
+  value = (value & ~offsetMask) | replacement;  // First, zero the nibble to be replaced, then replace it with the new nibble
 
-    cout << hex << value << '\n';                       // show the new value
+  cout << hex << value << '\n';                 // show the new value
 }
