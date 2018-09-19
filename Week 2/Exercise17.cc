a8a049e8b1a7bbc8b1592edce02ebd03cafc9fad
EmielK <EmielKrol@gmail.com>
2018-09-19 20:26:36 +0200

    yeeees

--- Week 2/Exercise17.cc
+++ Week 2/Exercise17.cc
@@ -13,6 +13,7 @@ using namespace std;
 int main(int argc, char const *argv[]) {
 
   string input = argv[1];                // the first argument is the binary number supplied
+
   size_t value = stoi(input,nullptr,2);  //Turning the string into an integer so we can calculate with it.
                                          //Where the 2 indicates that the string is a binary number.
   size_t newValue;
@@ -23,61 +24,59 @@ int main(int argc, char const *argv[]) {
 
   string stringrorofrol = argv[2];       //Saving argv[2] in a string to test whether it is rol or ror
 
-  if(stringrorofrol=="ror")              //Checking whether
-  {
+  if(stringrorofrol=="ror")              //Checking whether argv[2] is ror
     caseint=0;
-  }
-  if(stringrorofrol=="rol")
-  {
-    caseint=1;
-  }
 
-  string newInput = input;
+  if(stringrorofrol=="rol")              //Checking whether argv[2] is rol
+    caseint=1;
+
+
 
   switch (caseint) {
-    case 0:
+    case 0:                       //if ror
       if(((value>>1)<<1)==value)  //if even
       {
-        //  1000 -> 0100 ror
+        //example 1000 -> 0100 ror
         newValue = (value>>1);
-        newInput.pop_back();      //int way
-        newInput.insert(0,"0");   //string way
         break;
       }
       if(((value>>1)<<1)!=value) //if uneven
       {
-        //1001 -> 1100 ror
-        newValue = ((value>>1)+pow(2,digits-1));
-        newInput.pop_back();
-        newInput.insert(0,"1");
+        //example 1001 -> 1100 ror
+        newValue = (value>>1)+pow(2,digits-1);
         break;
       }
-    case 1:
-      if((input[0])=='1') //if most significant digit 1
+    case 1:               //if rol
+      if((value-pow(2,digits-1))>= 0) //if most significant digit 1
       {
-        // 1000 -> 0001 lol
-        newValue = ((value<<1)+1-pow(2,digits));
-        newInput.erase(0,1);
-        newInput.append("1");
+        // example 1000 -> 0001 rol
+        newValue = (value<<1)+1-pow(2,digits);
         break;
       }
-      if((input[0])=='0') //if most significant digit 0
+      else //if most significant digit 0
       {
-        // 0101 -> 1010 lol
+        // example 0101 -> 1010 rol
         newValue = (value<<1);
-        newInput.erase(0,1);
-        newInput.append("0");
         break;
       }
     break;
     }
 
-  stringstream hexS;
-  hexS << hex << newValue;      //Getting the hexadecimal form.
-  string hexV (hexS.str() );    //Turning it into a string
+  string newBit;
+  size_t bitV = newValue;
 
-  size_t testvalue = stoi(newInput,nullptr,2);
+  for (digits; 0 < digits; digits--) {
+    if(bitV-pow(2,digits-1)<0)
+    {
+      newBit.append("0");
+    }
+    if(bitV-pow(2,digits-1)>=0)
+    {
+      newBit.append("1");
+      bitV = bitV - pow(2,digits-1);
+    }
+  }
 
-  cout << newInput << ' ' << newValue << ' ' << hexV << '\n'; //Printing the value in binary - decimal and hexadecimal
-  cout << testvalue << '\n';
+  //Printing the value in binary - decimal and hexadecimal
+  cout << newBit << ' ' << newValue << ' ' << hex << newValue << '\n';
 }
