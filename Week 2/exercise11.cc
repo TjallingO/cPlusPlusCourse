d0f937dd6b50d9edd9de0311819a0aea792d3d76
TjallingO <tjalling@taikie.com>
2018-09-21 16:17:39 +0200

    ex11 corrected

--- Week 2/exercise11.cc
+++ Week 2/exercise11.cc
@@ -7,9 +7,9 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-  size_t number = stoul(argv[1]);
+  size_t number = stoul(argv[1]);             // Initialise and enter the integer
 
-  for(size_t index = 1; index != 11; index++)
-    cout << index << " * " << number << " = "
-         << (index * number) << '\n';
+  for(size_t index = 1; index != 11; ++index) // For 1-10
+    cout << index << " * " << number << " = " // Display those ...
+         << index * number << '\n';           // ... multiplied by the integer
 }
