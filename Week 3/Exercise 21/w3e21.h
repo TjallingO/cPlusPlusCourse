f9a2fa47cfb8b4fcd5b42be75123544ce24adf17
EmielK <EmielKrol@gmail.com>
2018-09-24 13:51:21 +0200

    21 bijna af

---
+++ Week 3/Exercise 21/w3e21.h
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+
+#ifndef SUMFUNCTIONS
+#define SUMFUNCTIONS
+
+#include <cstddef> //nodig?
+
+void structCall(int nr, char const *arguments[]);
+void structCall();
+void boundCall(int nr, char const *arguments[]);
+void boundCall();
+
+#endif
