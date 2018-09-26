e28334d76b41040d2a16484d8fe1691f90a29d35
TjallingO <tjalling@taikie.com>
2018-09-26 23:47:38 +0200

    e23 update

--- w3/23/main.cc
+++ w3/23/main.cc
@@ -6,23 +6,22 @@
 #include "main.ih"
 
 using namespace std;
+extern char **environ;
+
 
 int main()
 {
-	string exampleArray[] =
-	{
-		"eAEGIERwrihxmVOAbKKxDmrLhKyKgr",
-		"CSmsswBbyPLTtWlFeIuhcjfQQm",
-		"l){Ly)54)EAE|2Sefj!cg(ZEg&fwkhtgSj4!mtYo:),Luj"
-	};
+	size_t arraySize = 0;	// Initialise size of needed array
 
-	for (size_t index = 0; index != 3; ++index)
+	while (environ[arraySize] != 0)	// Find out number of env variables
+		++arraySize;
+
+	string environArray[arraySize];	// Initialise string array of that size
+
+	for (size_t index = 0; environ[index] != 0; ++index)	// For every env var
 	{
-		quicksort(exampleArray[index], 0, exampleArray[index].size() - 1);	// Sort string
-		cout << exampleArray[index] << "\n"; // Output (now sorted) string
+		string environVariable = environ[index];	// Create a temp string for it
+		quicksort(environVariable, 0, environVariable.size() - 1);	// Sort that string
+		environArray[index] = environVariable;	// And add it to the string array
 	}
-	// Output:
-	// AAbDeEEGghhiIKKKKLmmORrrrVwxxy
-	// BbCceFfhIjLlmmPQQssSTtuwWy
-	// !!&()))),2445:AcEEeEffggghjjjklLLmoSSttuwyYZ{|
 }
