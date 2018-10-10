0bd36d08b1b20cd41e66e6ea0da1f93ba74f31b0
TjallingO <tjalling@taikie.com>
2018-10-10 23:59:50 +0200

    update

--- w4/33-34/person/extract.cc
+++ w4/33-34/person/extract.cc
@@ -5,24 +5,26 @@
 void Person::extract(istream &inputStream)
 {
   string varValue; // Initialise string to be used to populate variables
-  for (size_t index = 1; index != 4; ++index) // Loop through first three vars
+  for (size_t index = 0; index != 3; ++index) // Loop through first three vars
   {
     if (!getline(inputStream, varValue, ',')) // Read until a comma is found,
       break;
     switch (index)                            // assign that to the string
     {                                         // and then assign the string
-      case 1:                                 // to the vars in order
+      case 0:                                 // to the vars in order
         setName(varValue);
         break;
-      case 2:
+      case 1:
         setAddress(varValue);
         break;
-      case 3:
+      case 2:
         setPhone(varValue);
         break;
-    }
+      default:                                // Should never happen, but
+        break;                                // it's good practice to include it,
+    }                                         // right?
   }
-  if (!getline(inputStream, varValue)) // For the last var, read until
-    return;                            // new-line char and assign it to mass
+  if (!getline(inputStream, varValue))        // For the last var, read until
+    return;                                   // new-line char and assign it to mass
   setMass(stoi(varValue));
 }
