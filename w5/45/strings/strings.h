ba59ecf9e1777e20982d3704a4c18f128e67b990
TjallingO <tjalling@taikie.com>
2018-10-15 03:27:52 +0200

    organisation

---
+++ w5/45/strings/strings.h
@@ -0,0 +1,16 @@
+#ifndef INCLUDED_STRINGS_
+#define INCLUDED_STRINGS_
+
+#include <cstdef>
+
+class Strings
+{
+  size_t d_size
+
+  public:
+    Strings();
+
+  private:
+};
+
+#endif
---
+++ w5/45/strings/strings1.cc
@@ -0,0 +1,6 @@
+#include "strings.ih"
+
+Strings::Strings(size_t numStrings, **strings)
+{
+  
+}
