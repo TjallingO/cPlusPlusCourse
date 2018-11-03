5b77b4d97680adf1c2bc0cd68f25dfb43c1420ba
Emiel Krol <emielkrol@gmail.com>
2018-11-03 18:46:01 +0100

    x

--- w7/66/main.cc
+++ w7/66/main.cc
@@ -11,10 +11,7 @@ int main(int argc, char **argv)
   string optionb;
   if (argv[3])
     optionb = argv[3];
-  //argv[1] input
-  //argv[2] output
-  //argv[3] = -b of niet
-  //cout << filetypecheck( inputLoc ) << '\n';
+
   switch( filetypecheck( inputLoc ) )
   {
     case CHARACTER :
@@ -32,50 +29,8 @@ int main(int argc, char **argv)
       bintochar(inputLoc, outputLoc);
     }
   }
-  /*
-  if (filetypecheck == CHARACTER)
-  {
-    if (optionb == "-b")
-      chartobin( inputLoc, outputLoc );
-    chartochar( inputLoc, outputLoc );
-  }
-  if (filetypecheck == BINARY)
-  {
-    if (optionb == "-b")
-      bintobin(inputLoc, outputLoc);
-    bintochar(inputLoc, outputLoc);
-  }
-  */
-
-
-  //input chars of convert naar chars
-
-
-  //if -b binary output, else human readable
-  //// human readable not actg return nonzero
-
-  //fstream io{ "./tmp/out", ios_base::out };
-  //ifstream input{ inputLoc };
-  //ofstream output{ "./tmp/out.txt", ios::trunc | ios::out };
-  //ofstream output{ "./tmp/out2.bin", ios::binary };
-
-  //cout << filesize( inputLoc ) << '\n';
 
 
 
 
-
-/*
-
-  char x;
-
-  while (input.get(x))
-  {
-    //output.write(reinterpret_cast<char const *>(&x), sizeof(x));
-    //output << (static_cast<char const>(x));
-    output << x;
-    //output.write((char*) &x, sizeof(x));
-
-  }
-*/
 }
