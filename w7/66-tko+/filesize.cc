09acaebe70ba06956261e541592c8795fad6eee8
Emiel Krol <emielkrol@gmail.com>
2018-11-14 17:05:40 +0100

    reset class changes

--- w7/66-tko+/filesize.cc
+++
@@ -1,8 +0,0 @@
-#include "main.ih" 
-
-std::ifstream::pos_type filesize(const string filename)
-{
-  std::ifstream input(filename, std::ifstream::ate | std:: ifstream::binary);
-  return input.tellg();
-  input.close();
-}
