7140262530ae4422e8719c129c8e0948ab29694e
EmielK <emielkrol@gmail.com>
2018-12-10 22:58:51 +0100

    22 ish 23 ish

--- II/w3/23/main.cc
+++ II/w3/23/main.cc
@@ -1,9 +1,21 @@
 #include "main.ih"
 
+void caller(Base &obj)
+{
+  Derived &tmp = static_cast < Derived &> (obj);
+  tmp.hello();
+}
+
+//Bad practise since now we have a tmp derived class which has no members
+//other than the base members so when it needs to use members which a derived
+//object should have it gives an error. Which is unexpected behaviour.
+
 int main(int argc, char const **argv)
 {
   Base myBase;
   myBase.hello();
   Derived myDerived;
   myDerived.hello();
+  myDerived.Base::hello();
+  caller(myBase);
 }
