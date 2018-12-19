1b7e58235d682f9fab28307eef216933fcb0dcfc
TjallingO <tjalling@taikie.com>
2018-12-20 00:27:15 +0100

    27 update

--- II/w4/27-tko/main.cc
+++ II/w4/27-tko/main.cc
@@ -5,14 +5,11 @@ int main(int argc, char const **argv)
 
   Base **bp = derivedFactory(10);
 
-  for (size_t idx = 0; idx < 10; ++idx) {
+  for (size_t idx = 0; idx != 10; ++idx)
     (*bp[idx]).hello(cout);
-  }
 
    for (size_t idx = 0; idx != 10; ++idx)
-   {
      delete bp[idx];
-   }
 
   delete[] bp;
 }
