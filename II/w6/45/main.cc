16f3ceeca2f2fa2596a634d601e51b14553310c2
TjallingO <tjalling@taikie.com>
2018-12-22 21:25:26 +0100

    45 update

--- II/w6/45/main.cc
+++ II/w6/45/main.cc
@@ -12,7 +12,18 @@ int main(int argc, char const **argv)
 
   cout << "Please enter delimited words to be sorted, end input with ^D \n";
 
-  copy(istream_iterator<string>(cin), istream_iterator<string>(), inserter(iStrings, iStrings.end()));
+  copy(
+       istream_iterator<string>(cin),
+       istream_iterator<string>(),
+       inserter(iStrings,iStrings.end())
+      );
 
-  copy(iStrings.begin(), iStrings.end(), ostream_iterator<string>(cout, " "));
+  // Or: set<string> iStrings((istream_iterator<string>(cin)),
+  //                          istream_iterator<string>());
+
+  copy(
+       iStrings.begin(),
+       iStrings.end(),
+       ostream_iterator<string>(cout, " ")
+      );
 }
