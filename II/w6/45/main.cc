3e9fc6f3f6ee95277dcb26a38f7aac96a2a48487
TjallingO <tjalling@taikie.com>
2018-12-22 21:17:56 +0100

    45

---
+++ II/w6/45/main.cc
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <set>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char const **argv)
+{
+  set<string> iStrings;
+
+  cout << "Please enter delimited words to be sorted, end input with ^D \n";
+
+  copy(istream_iterator<string>(cin), istream_iterator<string>(), inserter(iStrings, iStrings.end()));
+
+  copy(iStrings.begin(), iStrings.end(), ostream_iterator<string>(cout, " "));
+}
