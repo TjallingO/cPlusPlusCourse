9dec129559064e11e7172b83b9691219d8179109
EmielK <emielkrol@gmail.com>
2019-01-10 13:39:40 +0100

    46 met 2 oplossingen, mag stoi?

--- II/w6/46/main.cc
+++ II/w6/46/main.cc
@@ -2,59 +2,46 @@
 #include <vector>
 #include <algorithm>
 #include <functional>
+#include <iterator>
 
 
 
 int main(int argc, char **argv)
 {
-  // arg1 number rvalues
-  // arg 2 max rv to generate
-  // arg 3 lookup value
-
   int nrRvalues = stoi(argv[1]);
   int maxRvalue = stoi(argv[2]);
   int lookupVal = stoi(argv[3]);
 
   vector<int> numbers;
-  int test = 3;
 
   for (size_t idx = 0; idx < nrRvalues; ++idx)
     numbers.push_back(random() % (maxRvalue + 1)); //adding a random number
-                                                    //between 0 and max_rvalue
-
+                                                   //between 0 and max_rvalue
   for (auto idx: numbers)
     cout << idx << '\t';
-
   cout << '\n';
 
-  //if ( find(numbers.begin(), numbers(end), lookupVal) != numbers.end())
-  //find_if( numbers.begin(), numbers.end(), (greater_equal<int>(), lookupVal));
+//manier 1
+  int upbound = *find(numbers.begin(), numbers.end(), lookupVal + 1);
+  auto it =
+  find(numbers.begin(), numbers.end(), upbound);
+  cout << "The first value exceeding "  << lookupVal << " is at index "
+  << distance(numbers.begin(), it)  << '\n';
 
-  size_t counter = 0;
-  bool boolloop = true;
+//manier 2, hier met lambda
+  auto it2 = find_if(numbers.begin(), numbers.end(),
+    [lookupVal](const int & val)
+      {
+        if (val > lookupVal)
+          return true;
+        return false;
+      }
+  );
 
-  for (auto idx: numbers)
-  {
-    if (idx >= lookupVal)
-    {
-      cout << "The first value exceeding " << lookupVal << " is at index "
-      << counter << '\n';
-      boolloop = false;
-      break;
-    }
-    else
-    {
-      ++counter;
-    }
-  }
-
-  if (boolloop == true)
-  {
+  if(it2 != numbers.end())
+    cout << "The first value exceeding " << lookupVal << " is at index "
+    << distance(numbers.begin(), it2) << '\n';
+  else
     cout << "No random value exceeds " << lookupVal << '\n';
-  }
 
-
-
-  //if found: The first value exceeding ... is at index ...
-  //else No random value exceeds ...
 }
