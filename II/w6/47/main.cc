689abf4f027e44d2582a8b499ac6b7e55ef4e2c0
TjallingO <tjalling@taikie.com>
2019-01-11 17:23:51 +0100

    47: weet niet of dit mag

---
+++ II/w6/47/main.cc
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+  sort(argv, argv + argc, [](char *left, char *right) // Sort ascending
+  {
+    return *left < *right;
+  });
+  copy(argv, argv + argc, ostream_iterator<string>(cout, " ")); // Print
+
+  cout << '\n'; // New line
+
+  sort(argv, argv + argc, [](char *left, char *right) // Sort descending
+  {
+    return *left > *right;
+  });
+  copy(argv, argv + argc, ostream_iterator<string>(cout, " ")); // Print
+}
