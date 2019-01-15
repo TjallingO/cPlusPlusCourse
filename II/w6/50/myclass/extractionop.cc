32f37415564eba85fdacc7f9f3ee8464b12f790f
EmielK <emielkrol@gmail.com>
2019-01-15 15:19:00 +0100

    xx

--- II/w6/50/myclass/extractionop.cc
+++ II/w6/50/myclass/extractionop.cc
@@ -6,15 +6,8 @@ istream &operator>>(istream &istr, vector<string> &vs)
   //while (getline(istr, line))
   //  vs.push_back(line);
 
+
   copy(istream_iterator<string>(istr), istream_iterator<string>(),
         back_inserter(vs));
 
 }
-/*
-istream &operator>>(istream &istr, vector<string> &vs)
-{
-  string line;
-  while (getline(istr, line))
-    vs.push_back(line);
-}
-*/
