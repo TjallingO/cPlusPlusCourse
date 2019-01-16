9a4c4d6be4835e0d68de6e30fab67083a1741bc7
TjallingO <tjalling@taikie.com>
2019-01-16 22:43:00 +0100

    50-2 update

--- II/w6/50-2/line/line.h
+++ II/w6/50-2/line/line.h
@@ -1,7 +1,6 @@
 #ifndef INCLUDED_LINE_
 #define INCLUDED_LINE_
 
-
 class Line : public std::string
 {
     friend std::istream &operator>>(std::istream &is, Line &line)
--- II/w6/50-2/main.cc
+++ II/w6/50-2/main.cc
@@ -13,7 +13,7 @@ int main(int argc, char **argv)
 
   cin >> vs;
 
-  for(auto it: vs)
+  for (auto it: vs)
     cout << it << '\n';
 }
 
