8fcecd118eed30de761312e2f1fbd5114ee861b3
TjallingO <tjalling@taikie.com>
2019-01-19 19:56:44 +0100

    55 prelim done

--- II/w7/55/main.cc
+++ II/w7/55/main.cc
@@ -3,43 +3,67 @@
 #include <vector>
 #include <iostream>
 #include <chrono>
+#include <thread>
+#include <algorithm>
 
-void isPrime(size_t &next, vector<size_t> &primes)
+void calcPrimes(size_t noPrimes, bool &calcDone, vector<size_t> &primes)
 {
-  for (auto el: primes)
+  size_t next = 3;
+
+  while (primes.size() < noPrimes)
   {
-    if ( (next % el) == 0 )
-    {
-      ++next;
-      return;
-    }
+    bool isPrime = any_of(
+                          primes.begin(),
+                          primes.end(),
+                          [&](auto el)
+                          {
+                            return next % el == 0;
+                          }
+                         );
+    if (!isPrime)
+      primes.push_back(next);
+    ++next;
   }
-  primes.push_back(next);
-  ++next;
+  calcDone = true;
 }
 
-void dotting()
+void dotting(size_t seconds, bool &calcDone)
 {
+  while (!calcDone)
+  {
+    std::this_thread::sleep_for(std::chrono::seconds(seconds));
+    cerr << '.';
+  }
 }
 
 int main(int argc, char const **argv)
 {
+  bool calcDone = false;
+  vector<size_t> primes{ 2 };
+
+  if (argc != 2)
+  {
+    cerr << "Please specify the number of primes to compute \n";
+    return 0;
+  }
   auto startChrono = chrono::system_clock::now();
   time_t start = chrono::system_clock::to_time_t(startChrono);
 
-  dotting();
-  vector<size_t> primes{ 2 };
-  size_t next = 3;
+  size_t noPrimes = stoi(argv[1]);
 
-  while (primes.size() != stoi(argv[1]))
-    isPrime(next, primes);
+  thread dottingThread(dotting, 1, ref(calcDone));
+  thread primesThread(calcPrimes, noPrimes, ref(calcDone), ref(primes));
+
+  primesThread.join();
+  dottingThread.join();
+
+  cout << '\n';
+  for (auto el: primes)
+    cout << el << ' ';
 
   auto endChrono = chrono::system_clock::now();
   time_t end = chrono::system_clock::to_time_t(endChrono);
   chrono::duration<double> elapsed_seconds = endChrono - startChrono;
-
-  for (auto el: primes)
-    cout << el << ' ';
 
   cout << "\nStarting time: " << ctime(&start)
        << "Ending time:   " << ctime(&end)
