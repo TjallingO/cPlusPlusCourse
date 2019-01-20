a73f7215113e4689cfa2b85ed02aebb00928df54
TjallingO <tjalling@taikie.com>
2019-01-20 17:27:35 +0100

    56 (but may not be the intended way)

---
+++ II/w7/56/main.cc
@@ -0,0 +1,24 @@
+#include "main.ih"
+
+#include <iostream>
+#include <chrono>
+
+int main(int argc, char const **argv)
+{
+  if (argc != 3)
+  {
+    cout << "Please pass two arguments.";
+    return 1;
+  }
+
+  chrono::hours   inputHours(stoi(argv[1]));
+  chrono::seconds inputSeconds(stoi(argv[2]));
+
+  chrono::minutes outputMinutesH = inputHours;
+  chrono::minutes outputMinutesS = chrono::duration_cast<chrono::minutes>(inputSeconds);
+
+  cout << inputHours.count()   << " hours equals " 
+                               << outputMinutesH.count() << " minutes \n"
+       << inputSeconds.count() << " seconds equals "
+                               << outputMinutesS.count() << " minutes";
+}
