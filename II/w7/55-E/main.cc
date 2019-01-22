7c05a59d32f37a7088b6633d1af315a0e953f3e6
EmielK <emielkrol@gmail.com>
2019-01-22 22:51:21 +0100

    ankdwa

---
+++ II/w7/55-E/main.cc
@@ -0,0 +1,54 @@
+#include "main.ih"
+
+void prime(bool done)
+{
+  vector<size_t> vs {2};
+  size_t next = 3;
+  size_t nrPrimes = 15;
+
+  while(vs.size() < nrPrimes)
+  {
+    if(find_if(vs.begin(), vs.end(),
+      [&next](const size_t &val)
+      {
+        if (next%val == 0)
+           return true;
+        else return false;
+
+      }) == vs.end())
+    {
+      vs.push_back(next);
+    }
+    ++next;
+  }
+
+  for(auto it: vs)
+    cout << it << ' ';
+
+  cout << '\n';
+
+  done = true;
+}
+
+void dots(bool done)
+{
+  using namespace std::chrono_literals;
+
+  while(done)
+  {
+    std::this_thread::sleep_for(1s);
+    using namespace std;
+    cout << ".";
+  }
+}
+
+int main(int argc, char **argv)
+{
+  bool done = false;
+
+  thread thr(prime, ref(done));
+  thread thr2(dots, ref(done));
+
+  thr.join();
+  thr2.join();
+}
