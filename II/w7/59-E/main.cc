51a19eebbbcebdc7820fd6329058ef270a79a633
EmielK <emielkrol@gmail.com>
2019-01-24 16:44:04 +0100

    aaaa

--- II/w7/59-E/main.cc
+++ II/w7/59-E/main.cc
@@ -12,11 +12,10 @@ int main(int argc, char const *argv[])
 {
   Storage warehouse(argv[1]);
 
-  //thread thr1(getlines, ref(cin), ref(warehouse));
-  //thread thr2(printlines, ref(warehouse));
-  thread runThread(&Storage::run, ref(warehouse));
+  thread runThread(&Storage::run, ref(warehouse)); //while there is more input
+  //to be processed adds lines to the queue
   thread printThread(&printlines, ref(warehouse), ref(cin));
-
+  //prints line by line to the file
 
   runThread.join();
   printThread.join();
--- II/w7/59-E/storage/getobject.cc
+++ II/w7/59-E/storage/getobject.cc
@@ -2,13 +2,9 @@
 
 string Storage::getobject()
 {
-  //cout << "getobject\n";
   unique_lock<mutex> ul(d_mutex);
   while (this -> empty())
-  {
-    //cout << "getobject 2\n";
     d_condition.wait(ul);
-  }
-  //cout << "getobject 3\n";
+
   return this -> next();
 }
