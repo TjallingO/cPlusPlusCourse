8e740833c4536ec4f8981c796067e77df9e3ffce
EmielK <emielkrol@gmail.com>
2019-01-30 19:29:05 +0100

    nu zoals in boek maar exception

---
+++ II/w8/64-2/main.cc
@@ -0,0 +1,49 @@
+#include "main.ih"
+
+double lhs[4][5] = {
+  {0, 1, 2, 3, 4},
+  {5, 6, 7, 8, 9},
+  {0, 1, 2, 3, 4},
+  {5, 6, 7, 8, 9}
+};
+
+double rhsT[6][5] = {
+  {0, 1, 2, 3, 4},
+  {5, 6, 7, 8, 9},
+  {0, 1, 2, 3, 4},
+  {5, 6, 7, 8, 9},
+  {5, 6, 7, 8, 9},
+  {0, 1, 2, 3, 4}
+};
+
+void innerProduct(promise<double> &ref, int row, int col)
+{
+  int sum = 0;
+  for (int idx = 0; idx != 5; ++idx)
+    sum += lhs[row][idx] * rhsT[col][idx];
+  cout << row << ' ' << col << '\n';
+  ref.set_value(sum);
+}
+
+int main(int argc, char const **argv)
+{
+
+  promise<double> result[6][4];
+
+  for (int row = 0; row != 4; ++row)
+  {
+    for (int col = 0; col != 6; ++col)
+      thread(innerProduct, ref(result[row][col]), row, col).detach();
+  }
+
+  for (int row = 0; row != 4; ++row)
+  {
+    for (int col = 0; col != 6; ++col)
+      cout << setw(3) << result[row][col].get_future().get();
+    cout << '\n';
+  }
+
+
+}
+
+//inner_product(lhs[row].begin(), lhs[row].end(), rhsT[col].begin(), 0);
