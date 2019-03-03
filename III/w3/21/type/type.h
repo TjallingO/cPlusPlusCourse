3248932f7c5c41262caf08daf57d8b9090e68b4c
EmielK <emielkrol@gmail.com>
2019-03-03 19:05:56 +0100

    start 21

--- III/w3/21/type/type.h
+++ III/w3/21/type/type.h
@@ -1,12 +1,22 @@
 #ifndef INCLUDED_TYPE_
 #define INCLUDED_TYPE_
 
+#include "../typeidx/typeidx.h"
+
 template<typename NeedleT, typename ...HayStackT>
 class Type
 {
   public:
 
+    enum
+    {
+      value = 0 + TypeIdx<NeedleT, HayStackT...>::value
+    };
+
   private:
 };
+
+
+
 
 #endif
--- III/w3/21/typeidx/typeidx.h
+++ III/w3/21/typeidx/typeidx.h
@@ -1,12 +1,32 @@
 #ifndef INCLUDED_TYPEIDX_
 #define INCLUDED_TYPEIDX_
 
-
+template<typename NeedleT, typename LastT>
 class TypeIdx
 {
   public:
 
+    enum
+    {
+      value = 1
+    };
+
   private:
 };
+
+template<typename NeedleT, typename next, typename ...RestT>
+class TypeIdx
+{
+  public:
+
+    enum
+    {
+      value = 1 + TypeIdx<NeedleT, RestT...>::value
+    };
+
+  private:
+};
+
+
 
 #endif
