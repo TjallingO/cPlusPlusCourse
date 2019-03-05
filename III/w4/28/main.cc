6ceb5f74b183236418794009d14c9110fd25ffbc
EmielK <emielkrol@gmail.com>
2019-03-05 14:35:04 +0100

    start 28

---
+++ III/w4/28/main.cc
@@ -0,0 +1,10 @@
+#include "main.ih"
+
+int main(int argc, char const **argv)
+{
+
+  cout << "plain :" << Basic<int>::value << '\n'
+       << "pointer :" << Basic<int*>::value << '\n'
+       << "reference :" << Basic<int&>::value << '\n'
+       << "rvalue reference :" << Basic<int&&>::value << '\n';
+}
