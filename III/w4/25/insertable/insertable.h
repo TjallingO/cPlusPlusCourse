4e00b40e9fea45ed86e2e61bd007e676e03889c6
TjallingO <tjalling@taikie.com>
2019-03-05 18:42:49 +0100

    25+26 without inclass defs, but with friend decl.

--- III/w4/25/insertable/insertable.h
+++ III/w4/25/insertable/insertable.h
@@ -1,6 +1,12 @@
 #ifndef INCLUDED_INSERTABLE_
 #define INCLUDED_INSERTABLE_
 
+#define HDR_  template <typename Data, \
+              template <typename, typename> class Container, \
+              template <typename> class AllocationPolicy>
+#define CONT_ Container<Data, AllocationPolicy<Data>>
+#define INS_  Insertable<Data, Container, AllocationPolicy>
+
 
 #include <ostream>
 #include <vector>
@@ -14,31 +20,33 @@ template <typename> class AllocationPolicy = std::allocator>
 class Insertable: public Container<Data, AllocationPolicy<Data>>
 {
   public:
-    using MyContainer = Container<Data, AllocationPolicy<Data>>;
 
-    Insertable(const MyContainer &RHS) : MyContainer(RHS) {};
-    Insertable(const Insertable &RHS) : MyContainer(RHS) {};
-    Insertable() : MyContainer() {};
-    Insertable(Data RHS): MyContainer(RHS) {};
+    Insertable(const CONT_ &RHS);
+    Insertable(const Insertable &RHS);
+    Insertable();
+    Insertable(Data RHS);
 
     friend std::ostream &operator<<(std::ostream &out, const Insertable &ins)
     {
       std::copy (ins.begin(), ins.end(), std::ostream_iterator<Data>(out, "\n"));
       return out;
     };
-
-
 };
 
-// template <>
-// inline Insertable::Insertable(const MyContainer &RHS) : MyContainer(RHS)
-// {};
-// inline Insertable::Insertable(const Insertable &RHS) : MyContainer(RHS)
-// {};
-// inline Insertable::Insertable() : MyContainer()
-// {};
-// inline Insertable::Insertable(Data RHS): MyContainer(RHS)
-// {};
+HDR_
+INS_::Insertable(const CONT_ &RHS) : CONT_(RHS)
+{};
+HDR_
+INS_::Insertable(const Insertable &RHS) : CONT_(RHS)
+{};
+HDR_
+INS_::Insertable() : CONT_()
+{};
+HDR_
+INS_::Insertable(Data RHS): CONT_(RHS)
+{};
 
-
+#undef HDR_
+#undef CONT_
+#undef INS_
 #endif
