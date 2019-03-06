00d89fa747b95ce97fc8abd98d263603ecb7aa9f
EmielK <emielkrol@gmail.com>
2019-03-06 18:08:52 +0100

    printing en wat verbeteringen 20, maar snap de feedback niet :(

--- III/w3/20-2/chars/chars.h
+++ III/w3/20-2/chars/chars.h
@@ -12,17 +12,16 @@ class Chars<First, CharsT...>
 {
   public:
 
-    static std::string letters()
-    {
-      return First + Chars<CharsT...>::letters();
-    }
+    static std::string letters();
+    //returns the first character and keeps appending letters from the param
+    //pack by calling itself without the the First char. Then once there is
+    //no more input the other letters() function from the <> specialization is
+    //called which does not call another function, ending the recursion.
 
+    template <char First2, char... CharsT2>
     friend std::ostream &operator<<(std::ostream &out,
-                                    Chars<First, CharsT...> const &rhs)
-    {
-        out << rhs.letters();
-      return out;
-    }
+                                    Chars<First, CharsT...> const &rhs);
+    //calls letters() and passes all characters to out
 };
 
 
@@ -31,11 +30,31 @@ class Chars<>
 {
   public:
 
-    static std::string letters()
-    {
-      return "";
-    }
+    static std::string letters();
+    //called when there are no more characters in the parameter pack,
+    //ending the recursion.
 };
+
+template <char First, char... CharsT>
+std::string Chars<First, CharsT...>::letters()
+{
+  return First + Chars<CharsT...>::letters();
+}
+
+
+template <char First, char... CharsT>
+std::ostream &operator<<(std::ostream &out,
+                                Chars<First, CharsT...> const &rhs)
+{
+  out << rhs.letters();
+  return out;
+}
+
+//template <>
+std::string Chars<>::letters()
+{
+  return "";
+}
 
 
 #endif
--- III/w3/21/type/type.h
+++ III/w3/21/type/type.h
@@ -9,14 +9,14 @@ class Type
   public:
 
     enum
-    {
+    { //TypeIdx starts looking at position 1 in the haystack
       located = 0 + TypeIdx<1, NeedleT, HayStackT...>::located
     };
 
   private:
 };
 
-template<typename NeedleT>
+template<typename NeedleT> //if there is only a needle eg Type<int>::located
 class Type<NeedleT>
 {
   public:
