35af767fde88cbbb6795c4e1749cde79f7c4c64c
EmielK <emielkrol@gmail.com>
2019-03-06 21:20:51 +0100

    20

--- III/w3/20-2/chars/chars.h
+++ III/w3/20-2/chars/chars.h
@@ -5,64 +5,32 @@
 
 template <char ...CharsT>
 class Chars
-{};
-
-template <char First, char... CharsT>
-class Chars<First, CharsT...>
 {
   public:
 
-    constexpr static std::string letters();
+  constexpr static std::string letters();
 
-    //returns the first character and keeps appending letters from the param
-    //pack by calling itself without the the First char. Then once there is
-    //no more input the other letters() function from the <> specialization is
-    //called which does not call another function, ending the recursion.
-
-    template <char First2, char... CharsT2>
-    friend std::ostream &operator<<(std::ostream &out,
-                                    Chars<First, CharsT...> const &rhs);
-    //calls letters() and passes all characters to out
+  template <char... CharsT2>
+  friend std::ostream &operator<<(std::ostream &out,
+                                  Chars<CharsT2...> const &rhs);
 };
 
 
-template <>
-class Chars<>
+template <char... CharsT>
+constexpr std::string Chars<CharsT...>::letters()
 {
-  public:
-
-    static std::string letters();
-    //called when there are no more characters in the parameter pack,
-    //ending the recursion.
-};
-
-template <char First, char... CharsT>
-constexpr std::string Chars<First, CharsT...>::letters()
-{
-  return std::string{ First, CharsT... };
-  //return First + Chars<CharsT...>::letters();
+  return std::string{ CharsT... };
 }
 
-// template <char Next, char... CharsT>
-// char Chars<Next, CharsT...>::letter()
-// {
-//   return Next;
-// }
 
-
-template <char First, char... CharsT>
+template <char... CharsT>
 std::ostream &operator<<(std::ostream &out,
-                                Chars<First, CharsT...> const &rhs)
+                                Chars<CharsT...> const &rhs)
 {
   out << rhs.letters();
   return out;
 }
 
-//template <>
-std::string Chars<>::letters()
-{
-  return "";
-}
 
 
 #endif
