50488df5b7b894a8e9bbb8c6040a8731f904ef42
TjallingO <tjalling@taikie.com>
2019-03-10 18:02:12 +0100

    36 update

--- III/w5/36/main.cc
+++ III/w5/36/main.cc
@@ -2,30 +2,36 @@
 
 int main(int argc, char const **argv)
 {
+  size_t curLine = 0;
   Scanner flatbed("whatever", "/dev/null");
   while (true)
   {
+    if (curLine != flatbed.lineNr())
+    {
+      curLine = flatbed.lineNr();
+      cout << "\nLine " << curLine << ": ";
+    }
     switch (flatbed.lex())
     {
       case 0:
         return 0;
       case INT:
-        cout << "INT:      " << flatbed.matched() << '\n';
+        cout << "INT: " << flatbed.matched() << ' ';
         break;
       case WORD:
-        cout << "WORD:     " << flatbed.matched() << '\n';
+        cout << "WORD: " << flatbed.matched() << ' ';
         break;
       case FLOAT:
-        cout << "FLOAT:    " << flatbed.matched() << '\n';
+        cout << "FLOAT: " << flatbed.matched() << ' ';
         break;
       case STRING:
-        cout << "STRING:   " << flatbed.matched() << '\n';
+        cout << "STRING: " << flatbed.matched() << ' ';
         break;
       case OP:
-        cout << "OPERATOR: " << flatbed.matched() << '\n';
+        cout << "OPERATOR: " << flatbed.matched() << ' ';
         break;
       default:
-        cout << "CHAR:     " << flatbed.matched() << '\n';
+        cout << "CHAR: " << flatbed.matched() << ' ';
         break;
     }
   }
