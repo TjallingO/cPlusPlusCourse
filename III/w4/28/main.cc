f0ac71e786819797bff39424adb7f28c457df15c
EmielK <emielkrol@gmail.com>
2019-03-13 22:54:46 +0100

    i

--- III/w4/28/main.cc
+++ III/w4/28/main.cc
@@ -5,7 +5,7 @@ int main(int argc, char const **argv)
   int plain = 3;
   auto ptr = &plain;
 
-  cout << "plain :" << BasicTraits<const int>::isPlainType << '\n'
+  cout << "plain :" << BasicTraits<int>::isPlainType << '\n'
        << "pointer :" << BasicTraits<int*>::isPointerType << '\n'
        << "reference :" << BasicTraits<int&>::isRefType << '\n'
        << "rvalue reference :" << BasicTraits<int&&>::isRRefType << '\n';
@@ -18,6 +18,5 @@ int main(int argc, char const **argv)
    cout << BTobject1.value() << '\n'
         << BTobject2.value() << '\n'
         << BTobject3.value() << '\n'
-        << BTobject4.value() << '\n'; //prints 3 regardless of template arg
-        //pain is niet const.....
+        << BTobject4.value() << '\n'; 
 }
