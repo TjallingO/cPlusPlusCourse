c812a442152da695823e7eced675b0811ce74a5f
TjallingO <tjalling@taikie.com>
2019-04-04 04:02:47 +0200

    prelim 'printing' update

--- III/w7/49-krol/parser/parser.h
+++ III/w7/49-krol/parser/parser.h
@@ -12,10 +12,10 @@
 #include <unordered_map>
 
 #undef Parser
-    // CAVEAT: between the baseclass-include directive and the
-    // #undef directive in the previous line references to Parser
+    // CAVEAT: between the baseclass-include directive and the 
+    // #undef directive in the previous line references to Parser 
     // are read as ParserBase.
-    // If you need to include additional headers in this file
+    // If you need to include additional headers in this file 
     // you should do so after these comment-lines.
 
 
@@ -28,7 +28,7 @@ class Parser: public ParserBase
     std::unordered_map<std::string, unsigned> d_symtab;
 
     bool d_display;
-
+    
     public:
         Parser() = default;
         Parser(bool run);
@@ -37,7 +37,7 @@ class Parser: public ParserBase
     private:
         void error();                   // called on (syntax) errors
         int lex();                      // returns the next token from the
-                                        // lexical scanner.
+                                        // lexical scanner. 
         void print();                   // use, e.g., d_token, d_loc
         void exceptionHandler(std::exception const &exc);
 
@@ -49,13 +49,12 @@ class Parser: public ParserBase
         void print_();
 
         void error(char const *msg);
-
+        
     // added functions for the calculator:
 
         void display(RuleValue &e);
         void done();
         void prompt();
-        void list() const;
 
         RuleValue &add(RuleValue &lvalue, RuleValue &rvalue);
         RuleValue &assign(RuleValue &lvalue, RuleValue &rvalue);
@@ -65,7 +64,7 @@ class Parser: public ParserBase
         RuleValue variable();
 
         double valueOf(RuleValue const &e);
-
+        
 };
 
 
